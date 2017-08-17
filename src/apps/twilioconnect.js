415dc2bbf58ddaf79a5415b6f575a646e64b133b
DHLAP\h <vdavidhammond@gmail.com>
2017-08-17 15:47:42 -0600

    Added in Twilio tot eh most basic level

---
+++ src/apps/twilioconnect.js
@@ -0,0 +1,14 @@
+var twilio = require('twilio');
+var twilInfo = require("./twilkey.js");
+var accountSid = twilInfo.id; // Your Account SID from www.twilio.com/console
+var authToken = twilInfo.token;   // Your Auth Token from www.twilio.com/console
+
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    body: 'Hello from Node',
+    to: '+17205177858',  // Text this number
+    from: '+17208993602' // From a valid Twilio number
+})
+.then((message) => console.log(message.sid + " " + message));
