28def5e633eb128ada918a59cdce320d1a44abfb
DHLAP\h <vdavidhammond@gmail.com>
2017-08-18 12:20:32 -0600

    Set up logic for both the SMS and EMAIL to accept attachments only if the logic is provided.

--- src/apps/mailgun.js
+++ src/apps/mailgun.js
@@ -3,15 +3,30 @@ var api_key = mailKeyObj.key;
 var path = require('path');
 var domain = mailKeyObj.domain;
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
-var filepath = path.join(__dirname, '../../public/assets/images/burger.png');
-var data = {
-  from: 'Excited User <me@samples.mailgun.org>',
-  to: mailKeyObj.email,
-  subject: 'This is another Mailgun Testin',
-  text: 'Hopefully this will be an awesome application and these emails will send important information to users.',
-  attachment: filepath
-};
+var filepath = ""
+// path.join(__dirname, '../../public/assets/images/burger.png');
+if (filepath === "") {
+  var data = {
+    from: 'Excited User <me@samples.mailgun.org>',
+    to: mailKeyObj.email,
+    subject: 'This is another Mailgun Testin',
+    text: 'Hopefully this does not have attachment'
+  };
 
-mailgun.messages().send(data, function (error, body) {
-  console.log(body);
-});
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+}
+else {
+  var data = {
+    from: 'Excited User <me@samples.mailgun.org>',
+    to: mailKeyObj.email,
+    subject: 'This is another Mailgun Testin',
+    text: 'Hopefully this has attachment',
+    attachment: filepath
+  };
+
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+}
--- src/apps/twilioconnect.js
+++ src/apps/twilioconnect.js
@@ -1,13 +1,28 @@
-var twilio = require('twilio');
 var twilInfo = require("./twilkeys.js");
-var accountSid = twilInfo.id; // Your Account SID from www.twilio.com/console
-var authToken = twilInfo.token;   // Your Auth Token from www.twilio.com/console
-var twilio = require('twilio');
-var client = new twilio(accountSid, authToken);
 
-client.messages.create({
-    body: 'A task has been updated under Project_2 and has been marked urgent needing immediate attention',
-    to: '+17205177858',  // Text this number
-    from: '+17208993602' // From a valid Twilio number
-})
-.then((message) => console.log(message.sid + " " + message));
+// Twilio Credentials
+const accountSid = twilInfo.id;
+const authToken = twilInfo.token;
+var mediaLink = ""
+
+// require the Twilio module and create a REST client
+const client = require('twilio')(accountSid, authToken);
+if (mediaLink === "") {
+  client.messages
+    .create({
+      to: '+17205177858',
+      from: '+17208993602',
+      body: 'This is the ship that made the Kessel Run in fourteen parsecs?'
+    })
+    .then(message => console.log(message.sid));
+}
+else {
+  client.messages
+    .create({
+      to: '+17205177858',
+      from: '+17208993602',
+      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+      mediaUrl: mediaLink,
+    })
+    .then(message => console.log(message.sid));
+}
