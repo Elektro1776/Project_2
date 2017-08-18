f26212f490800d8355b145f130e0cc5033f01133
DHLAP\h <vdavidhammond@gmail.com>
2017-08-18 06:33:05 -0600

    removed MMS - doesnt work, not necessary -too expensive - attachments can be sent by email.  Working on chanign number to name still

--- src/apps/twilioconnect.js
+++ src/apps/twilioconnect.js
@@ -2,12 +2,11 @@ var twilio = require('twilio');
 var twilInfo = require("./twilkeys.js");
 var accountSid = twilInfo.id; // Your Account SID from www.twilio.com/console
 var authToken = twilInfo.token;   // Your Auth Token from www.twilio.com/console
-
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
 client.messages.create({
-    body: 'Hello from Node',
+    body: 'A task has been updated under Project_2 and has been marked urgent needing immediate attention',
     to: '+17205177858',  // Text this number
     from: '+17208993602' // From a valid Twilio number
 })
