7f7778a00467175b0e259314281ded6e4d6db305
DHLAP\h <vdavidhammond@gmail.com>
2017-08-17 20:42:09 -0600

    Email will now send with attachments.

--- src/apps/mailgun.js
+++ src/apps/mailgun.js
@@ -1,13 +1,15 @@
 var mailKeyObj = require("./mailkey.js");
 var api_key = mailKeyObj.key;
+var path = require('path');
 var domain = mailKeyObj.domain;
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
-
+var filepath = path.join(__dirname, '../../public/assets/images/burger.png');
 var data = {
   from: 'Excited User <me@samples.mailgun.org>',
   to: mailKeyObj.email,
   subject: 'This is another Mailgun Testin',
-  text: 'Hopefully this will be an awesome application and these emails will send important information to users.'
+  text: 'Hopefully this will be an awesome application and these emails will send important information to users.',
+  attachment: filepath
 };
 
 mailgun.messages().send(data, function (error, body) {
