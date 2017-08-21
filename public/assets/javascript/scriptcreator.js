7ec756004b372c5b9b94b03cdc24cd5d8be6bc37
DHLAP\h <vdavidhammond@gmail.com>
2017-08-21 14:37:17 -0600

    Got code working where user can type in text and it shows the code to create it and a snippet creator with dynamic syntax highlighting

--- public/assets/javascript/scriptcreator.js
+++ public/assets/javascript/scriptcreator.js
@@ -1,10 +1,5 @@
 $(document).ready(()=>{
-  $("#codeButt").click(()=>{
-    alert("I work");
-    $('#code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
-  });
+  
 $(function(){
   $('#edit').on('froalaEditor.contentChanged froalaEditor.initialized', function (e, editor) {
     $('#preview').text(editor.codeBeautifier.run(editor.html.get()))
