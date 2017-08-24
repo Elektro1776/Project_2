7b523e2b1209e957bd1fe035458ce83272860d53
DHLAP\h <vdavidhammond@gmail.com>
2017-08-24 11:36:26 -0600

    begining to add in github routes need to be up to date to fetch

---
+++ src/api/githubRoutes/githubCreate.js
@@ -0,0 +1,7 @@
+PUT -  CREATE FILE
+
+ const express = require('express');
+
+ const router = express.Router();
+
+ router.put('https://api.github.com/repos/:owner/:repo/contents/:path', {"owner":, "", "repo": "", "path": "", "message": "", "content": ""});
