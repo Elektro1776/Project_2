06766e21b575464bd78cbe0621154a9fa4025fd0
Papa Bravo Kilo <EchoChamber@Papas-MacBook-Pro.local>
2017-08-27 19:49:11 -0600

    Initial + Github Federation

--- src/auth/gitHubAuth.js
+++ src/auth/gitHubAuth.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 var express = require('express');
 var GitHubStrategy = require('passport-github2').Strategy;
 var github = require('./ghkey.js');
+var orm = require('../db/orm');
+
 module.exports = passport.use(new GitHubStrategy({
     clientID: github.id,
     clientSecret: github.secret,
@@ -9,11 +11,19 @@ module.exports = passport.use(new GitHubStrategy({
   },
   function(accessToken, refreshToken, profile, done) {
     if (profile) {
-      user = profile;
+      const {id,username} = profile;
       console.log(accessToken, " access token");
       console.log(refreshToken, " refresh token");
       console.log(profile, " profile");
-      return done(null, user);
+      const user = {
+        initial_federation: 'github',
+        utile_username: username,
+        github_federation_id: id,
+      }
+      orm.createUser(user).then((auth) => {
+       console.log(auth);
+       return done(null, username);
+      });
     }
     else {
       return done(null, false);
