a68f56d48f7e50cbe9cf30de9036dd12cebc9142
DHLAP\h <vdavidhammond@gmail.com>
2017-08-29 16:02:33 -0600

    Integrated command sheat sheet

--- public/assets/javascript/issues.js
+++ public/assets/javascript/issues.js
@@ -13,9 +13,10 @@ $(document).ready(()=>{
     $.post('/api/github/getIssues', currentObj, function (data) {
       console.log(data);
         for (issues in data) {
-          for (users in data[issues].assignees) {
-              collapseChild.children('.issueTextAppend').append('<p>Title: ' + data[issues].title + '<span>   |   </span>Created By: <a href="https://github.com/' + data[issues].user.login + '">' + data[issues].user.login + '</a><span>   |   </span>Issue #: ' + data[issues].number + '<span>   |   </span>Assignees: <a href="https://github.com/' + data[issues].assignees[users].login + '">' + data[issues].assignees[users].login + '</a></p><p>Body: ' + data[issues].body + '</p><hr/>');
-          }
+          collapseChild.children('.issueTextAppend').append('<p>Title: ' + data[issues].title + '</p>');
+          // for (users in data[issues].assignees) {
+          //     collapseChild.children('.issueTextAppend').append('<p>Title: ' + data[issues].title + '<span>   |   </span>Created By: <a href="https://github.com/' + data[issues].user.login + '">' + data[issues].user.login + '</a><span>   |   </span>Issue #: ' + data[issues].number + '<span>   |   </span>Assignees: <a href="https://github.com/' + data[issues].assignees[users].login + '">' + data[issues].assignees[users].login + '</a></p><p>Body: ' + data[issues].body + '</p><hr/>');
+          // }
         }
     });
 
