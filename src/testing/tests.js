720eff37e94d8d0c0bf5932963e895dde303b484
Austin <elektricwebdesign@gmail.com>
2017-09-02 12:39:48 -0600

    testing is added test is failing though, something with not handling async stuff correctly

--- src/testing/tests.js
+++ src/testing/tests.js
@@ -1,22 +1,48 @@
 require("mocha-generators").install();
 const Nightmare = require("nightmare");
 const expect = require("chai").expect;
+//
+// describe("Test Login", function() {
+//   it("Should be able to auth a user", function*(done) {
+//      this.timeout(12000);
+//     var nightmare = Nightmare({ show: true });
+//      yield nightmare
+//       .goto("http://localhost:3000")
+//       .click('a[href="/auth/github"')
+//       .type('input[id="login_field"]', "utile.testing@gmail.com")
+//       .type('input[id="password"]', "thisisfortestinggithub")
+//       .click('input[value="Sign in"]')
+//       .wait("div.jumbotron")
+//       .evaluate(function() {
+//         document.location.href;
+//       })
+//       .end()
+//       .then((link) => {
+//         console.log(' WHAT I THE RESULT', result);
+//         expect(link).to.equal("http://localhost:3000/");
+//         done()
+//       })
+//       // .end()
+//       // .end();
+//     // expect(link).to.equal("http://localhost:3000/");
+//   });
+// });
 
-describe("Test Login", function() {
-  it("Should be able to auth a user", function*(done) {
-    this.timeout(12000);
-    var nightmare = Nightmare({ show: true });
-    var link = yield nightmare
-      .goto("http://localhost:3000")
-      .click('a[href="/auth/github"')
-      .type('input[id="login_field"]', "utile.testing@gmail.com")
-      .type('input[id="password"]', "thisisfortestinggithub")
-      .click('input[value="Sign in"]')
-      .wait("div.jumbotron")
-      .evaluate(function() {
-        return document.location.href;
+describe('test duckduckgo search results', () => {
+  it('should find the nightmare github link first', (done) => {
+    const nightmare = Nightmare()
+    nightmare
+      .goto('https://duckduckgo.com')
+      .type('#search_form_input_homepage', 'github nightmare')
+      .click('#search_button_homepage')
+      .wait('#zero_click_wrapper .c-info__title a')
+      .evaluate(() =>
+        document.querySelector('#zero_click_wrapper .c-info__title a').href
+      )
+      .end()
+      .then((link) => {
+        expect(link).to.equal('https://github.com/segmentio/nightmare');
+        done();
       })
-      .end();
-    expect(link).to.equal("http://localhost:3000/");
   });
 });
